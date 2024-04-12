3972ad96dcf643940d660ddbace24b07aa468b70
Juan Claudio Valenzuela <jvalenzuelaortega@gmail.com>
2024-04-12 19:24:28 -0400

    TEST: base test classes are added

---
+++ financialindicators/app/src/test/java/financialindicators/services/FinancialIndicatorServiceTest.java
@@ -0,0 +1,12 @@
+package financialindicators.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FinancialIndicatorServiceTest {
+
+    @Test
+    void getIndicatorOfName() {
+    }
+}
---
+++ financialindicators/app/src/test/java/financialindicators/utils/JsoupScrapingUtilsTest.java
@@ -0,0 +1,12 @@
+package financialindicators.utils;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JsoupScrapingUtilsTest {
+
+    @Test
+    void getElementBySite() {
+    }
+}

382d54b676fa77912f4053a9a6871638d5c41563
Juan Claudio Valenzuela <jvalenzuelaortega@gmail.com>
2024-04-16 22:56:14 -0400

    FEATURE: add test for main and services indicators

--- financialindicators/app/src/test/java/financialindicators/AppTest.java
+++ financialindicators/app/src/test/java/financialindicators/AppTest.java
@@ -1,11 +1,55 @@
 package financialindicators;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.LocalDate;
+
 import org.junit.jupiter.api.Test;
 
+import financialindicators.dto.input.IndicatorInputDto;
+import financialindicators.dto.output.IndicatorOutputDto;
+import financialindicators.services.FinancialIndicatorService;
+import financialindicators.services.MapperIndicatorService;
+
 class AppTest {
+	
     @Test
-    void main() {
-        
+    void main_whenIndicatorIsDollar_thenReturnIndicator() {
+    	MapperIndicatorService mapperIndicatorService = new MapperIndicatorService();
+    	IndicatorInputDto indicatorInputDollarDto = mapperIndicatorService.buildIndicatorRequestDto("dollar", "vd");
+    	FinancialIndicatorService financialIndicatorService = new FinancialIndicatorService();
+    	
+    	IndicatorOutputDto indicator = financialIndicatorService.getIndicatorOfName(indicatorInputDollarDto);
+    	
+    	assertEquals(indicator.getIndicatorName(), "DOLLAR");
+    	assertEquals(indicator.getLocalDate(), LocalDate.now());
+    	assertEquals(indicator.getValue(), " $ 980.46");
+    }
+    
+    @Test
+    void main_whenIndicatorIsEuro_thenReturnIndicator() {
+    	MapperIndicatorService mapperIndicatorService = new MapperIndicatorService();
+    	IndicatorInputDto indicatorInputDollarDto = mapperIndicatorService.buildIndicatorRequestDto("euro", "vd");
+    	FinancialIndicatorService financialIndicatorService = new FinancialIndicatorService();
+    	
+    	IndicatorOutputDto indicator = financialIndicatorService.getIndicatorOfName(indicatorInputDollarDto);
+    	
+    	assertEquals(indicator.getIndicatorName(), "EURO");
+    	assertEquals(indicator.getLocalDate(), LocalDate.now());
+    	assertEquals(indicator.getValue(), " $ 1042.04");
+    }
+    
+    @Test
+    void main_whenIndicatorIsUf_thenReturnIndicator() {
+    	MapperIndicatorService mapperIndicatorService = new MapperIndicatorService();
+    	IndicatorInputDto indicatorInputDollarDto = mapperIndicatorService.buildIndicatorRequestDto("uf", "vpr");
+    	FinancialIndicatorService financialIndicatorService = new FinancialIndicatorService();
+    	
+    	IndicatorOutputDto indicator = financialIndicatorService.getIndicatorOfName(indicatorInputDollarDto);
+    	
+    	assertEquals(indicator.getIndicatorName(), "UF");
+    	assertEquals(indicator.getLocalDate(), LocalDate.now());
+    	assertEquals(indicator.getValue(), "$ 37.192,63");
     }
 
 }
